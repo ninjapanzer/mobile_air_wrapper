ed9afe878480660c7d38ea197a8a75c5c51d3ea4
Paul Scarrone <pscarrone@thinkthroughmath.com>
2013-09-17 10:08:38 -0400

    Removing Main.swf as it is a compiled binary and added some nice documentation to help setup the project when executing from the CompileAndRun shell command

--- CompileAndRunAS3-as.sh
+++ CompileAndRunAS3-as.sh
@@ -1,6 +1,29 @@
 #!/bin/sh
 
+RUNTIME_ARCHIVE="https://docs.google.com/a/thinkthroughmath.com/file/d/0B6jQ5p6g8RtodVVuYUwtQmhWVFk/edit?usp=sharing"
 
-amxmlc Main.as
+FLEX_LOC=runtimes/flex_sdk_4.6/bin
+AIR_LOC=runtimes/AdobeAIRSDK/bin
 
-adl sideload-app-as3-as.xml
+FLEX=`which $FLEX_LOC/amxmlc`
+AIR=`which $AIR_LOC/adl`
+
+if [[ "$FLEX" == "" || "$AIR" == "" ]]; then
+  echo "\nLooking for FLEX in $FLEX_LOC"
+  echo "Looking for AIR in $AIR_LOC"
+  echo "\nSorry Couldn't find the runtimes to build and execute this app"
+  echo "What I am looking for is 'amxmlc' and 'adl' from FLEX and AIR respectively"
+  echo "\nIf you don't know what I am talking about please Download\n$RUNTIME_ARCHIVE\nand extract it into the runtimes directory\n"
+  echo "\nThe Directory structure should looke like"
+  echo "| <Project Root>"
+  echo " \\"
+  echo " | runtimes"
+  echo "  \\"
+  echo "  | <AirSDK>"
+  echo "  | <FlexSDK>"
+  echo "\nFor extra configuration edit the FLEX_LOC and AIR_LOC of this file\n\n\n"
+else
+  runtimes/flex_sdk_4.6/bin/amxmlc Main.as
+
+  runtimes/AdobeAIRSDK/bin/adl sideload-app-as3-as.xml
+fi
