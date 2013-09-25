b3bd98d5ef1649388fa7496594f00bc681326c06
Paul Scarrone <pscarrone@thinkthroughmath.com>
2013-09-25 11:13:52 -0400

    Build system upgrades

--- CompileAndRunAS3-as.sh
+++ CompileAndRunAS3-as.sh
@@ -24,7 +24,7 @@ if [[ "$FLEX" == "" || "$AIR" == "" ]]; then
   echo "  | <FlexSDK>"
   echo "\nFor extra configuration edit the FLEX_LOC and AIR_LOC of this file\n\n\n"
 else
-  $FLEX_LOC/amxmlc Main.as
+  $FLEX_LOC/amxmlc Main.as -includes=mx.managers.systemClasses.MarshallingSupport
 
   $AIR_LOC/adl ttm_mobile-app-as3-as.xml
 fi
--- build_ios_debug.sh
+++ build_ios_debug.sh
@@ -5,6 +5,8 @@ FLEX_LOC=runtimes/flex_sdk_4.6/bin
 KEY_LOC=keys/ios
 BUILD_LOC=builds/ios
 
+$FLEX_LOC/amxmlc Main.as
+
 $AIR_LOC/adt -package -target ipa-debug \
 -keystore $KEY_LOC/ttm_app_key.p12 -storetype pkcs12 -storepass Apangea%123 \
 -provisioning-profile $KEY_LOC/TTM_AIR_ADHOC_Mobile.mobileprovision \
